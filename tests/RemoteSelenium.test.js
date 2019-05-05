0afe6329c97428b0185d8dce52cebee6980b0501
JohannC <johann.corcuff@gmail.com>
2019-05-05 10:53:08 +0200

    Trying new webdriver test

--- tests/RemoteSelenium.test.js
+++ tests/RemoteSelenium.test.js
@@ -8,8 +8,8 @@ var webdriver = require('selenium-webdriver'),
 var remoteHub = "http://" + username + ":" + authkey + "@localhost:4445";
 var browsers = [
     { browserName: 'Chrome', platform: 'Windows 10', version: '64', screen_resolution: '1366x768' },
-    { browserName: 'Chrome', platform: 'Mac OSX 10.14', version: '71x64', screen_resolution: '1366x768' },
-    { browserName: 'Internet Explorer', platform: 'Windows 8.1', version: '11', screen_resolution: '1366x768' }
+    //{ browserName: 'Chrome', platform: 'Mac OSX 10.14', version: '71x64', screen_resolution: '1366x768' },
+    //{ browserName: 'Internet Explorer', platform: 'Windows 8.1', version: '11', screen_resolution: '1366x768' }
 ];
 
 var flows = browsers.map(function (browser) {
@@ -24,35 +24,28 @@ var flows = browsers.map(function (browser) {
         password: authkey
     };
     it("Simple google test", async function parallelExample() {
-        try {
-            var driver = new webdriver.Builder()
-                .usingServer(remoteHub)
-                .withCapabilities(caps)
-                .build();
+        var driver = new webdriver.Builder()
+            .usingServer(remoteHub)
+            .withCapabilities(caps)
+            .build();
 
-            await driver.getSession().then(function (session) {
-                var sessionId = session.id_; //need for API calls
-                console.log('Session ID: ', sessionId);
-                console.log('See your test run at: https://app.crossbrowsertesting.com/selenium/' + sessionId)
-            });
+        await driver.getSession().then(function (session) {
+            var sessionId = session.id_; //need for API calls
+            console.log('Session ID: ', sessionId);
+            console.log('See your test run at: https://app.crossbrowsertesting.com/selenium/' + sessionId)
+        });
 
-            await driver.get('http://www.google.com');
-            var element = await driver.findElement(webdriver.By.name('q'));
-            await element.sendKeys('cross browser testing');
-            await element.submit();
-            await driver.getTitle().then(function (title) {
-                console.log("The title is: " + title);
-                if (title !== ('cross browser testing - Google Search')) {
-                    throw Error('Unexpected title: ' + title);
-                }
-            });
-            driver.quit();
+        await driver.get('http://www.google.com');
+        var element = await driver.findElement(webdriver.By.name('q'));
+        await element.sendKeys('cross browser testing');
+        await element.submit();
+        await driver.getTitle().then(function (title) {
+            console.log("The title is: " + title);
+            if (title !== ('cross browser testing - Google Search')) {
+                throw Error('Unexpected title: ' + title);
+            }
+        });
+        driver.quit();
 
-
-        }
-        catch (err) {
-            console.error('Exception!\n', err.stack, '\n');
-            driver.quit();
-        }
     });
 });
