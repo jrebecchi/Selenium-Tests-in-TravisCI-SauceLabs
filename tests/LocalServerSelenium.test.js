08b81d8f6e67842eed1f32fecd038fc93c1ef3c1
JohannC <johann.corcuff@gmail.com>
2019-05-08 22:04:19 +0200

    Testing local server

--- tests/LocalServerSelenium.test.js
+++ tests/LocalServerSelenium.test.js
@@ -1,15 +1,15 @@
-var username = 'JohannC'; //replace with your email address 
-var authkey = '22e26941-27d1-4713-aa3a-cacb0fd80ae7'; //replace with your authkey
+const username = 'JohannC'; //replace with your email address 
+const authkey = '22e26941-27d1-4713-aa3a-cacb0fd80ae7'; //replace with your authkey
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 30
 
 
-var webdriver = require('selenium-webdriver'),
+const webdriver = require('selenium-webdriver'),
     SeleniumServer = require('selenium-webdriver/remote').SeleniumServer,
     request = require('request');
 
-var remoteHub = "http://" + username + ":" + authkey + "@localhost:4445/wd/hub";
+    const remoteHub = "http://" + username + ":" + authkey + "@localhost:4445/wd/hub";
 
-var browsers = [
+const browsers = [
     { browserName: 'Chrome', platform: 'Windows 10', version: '64', screen_resolution: '1920x1080' },
     { browserName: 'Safari', platform: 'macOS 10.14', version: '12', screen_resolution: '1920x1440' },
     { browserName: 'Internet Explorer', platform: 'Windows 8.1', version: '11', screen_resolution: '1920x1080' },
@@ -18,7 +18,7 @@ var browsers = [
 
 browsers.map(function (browser) {
 
-    var caps = {
+    const caps = {
         name: 'Node Example',
         browserName: browser.browserName,
         version: browser.version,
@@ -28,29 +28,26 @@ browsers.map(function (browser) {
         password: authkey,
         'tunnel-identifier': process.env.TUNNEL_IDENTIFIER,
     };
-    it("Simple google test - "+browser.browserName, async function () {
+    it("Simple google test - "+browser.browserName, async () => {
         try {
-            var driver = new webdriver.Builder()
+            const driver = new webdriver.Builder()
                 .usingServer(remoteHub)
                 .withCapabilities(caps)
                 .build();
 
-            await driver.getSession().then(function (session) {
+            await driver.getSession().then((session) => {
                 var sessionId = session.id_; //need for API calls
                 console.log('Session ID: ', sessionId);
                 //console.log('See your test run at: https://app.crossbrowsertesting.com/selenium/' + sessionId)
             });
 
-            await driver.get('http://www.google.com');
-            var element = await driver.findElement(webdriver.By.name('q'));
+            await driver.get('http://localhost:39500');
+            const element = await driver.findElement(webdriver.By.name('search'));
             await element.sendKeys('cross browser testing');
-            await element.submit();
-            await driver.getTitle().then(function (title) {
-                console.log("The title is: " + title);
-                if (title !== ('cross browser testing - Google Search')) {
-                    throw Error('Unexpected title: ' + title);
-                }
-            });
+            const searchButton = await driver.findElement(webdriver.By.id('search_button'));
+            await searchButton.click();
+            const result = await driver.findElement(webdriver.By.id('result'));
+            console.log(result);
             driver.quit();
 
         }
